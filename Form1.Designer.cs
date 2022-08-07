d28190392af1ea10552319a763615c8aa38b46e1
esm720 <esm720@gmail.com>
2022-08-07 08:32:25 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Activity_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RollDiceButton = new System.Windows.Forms.Button();
+            this.Dice1Label = new System.Windows.Forms.Label();
+            this.Dice2Label = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RollDiceButton
+            // 
+            this.RollDiceButton.Location = new System.Drawing.Point(48, 68);
+            this.RollDiceButton.Name = "RollDiceButton";
+            this.RollDiceButton.Size = new System.Drawing.Size(163, 118);
+            this.RollDiceButton.TabIndex = 0;
+            this.RollDiceButton.Text = "Roll Dice";
+            this.RollDiceButton.UseVisualStyleBackColor = true;
+            this.RollDiceButton.Click += new System.EventHandler(this.RollDiceButton_Click);
+            // 
+            // Dice1Label
+            // 
+            this.Dice1Label.BackColor = System.Drawing.Color.Green;
+            this.Dice1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dice1Label.Location = new System.Drawing.Point(276, 64);
+            this.Dice1Label.Name = "Dice1Label";
+            this.Dice1Label.Size = new System.Drawing.Size(146, 122);
+            this.Dice1Label.TabIndex = 1;
+            this.Dice1Label.Text = "0";
+            this.Dice1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Dice2Label
+            // 
+            this.Dice2Label.BackColor = System.Drawing.Color.Green;
+            this.Dice2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dice2Label.Location = new System.Drawing.Point(485, 68);
+            this.Dice2Label.Name = "Dice2Label";
+            this.Dice2Label.Size = new System.Drawing.Size(146, 122);
+            this.Dice2Label.TabIndex = 2;
+            this.Dice2Label.Text = "0";
+            this.Dice2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ExitButton.Location = new System.Drawing.Point(283, 230);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(142, 60);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.RollDiceButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.CancelButton = this.ExitButton;
+            this.ClientSize = new System.Drawing.Size(698, 320);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.Dice2Label);
+            this.Controls.Add(this.Dice1Label);
+            this.Controls.Add(this.RollDiceButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RollDiceButton;
+        private System.Windows.Forms.Label Dice1Label;
+        private System.Windows.Forms.Label Dice2Label;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Enrique Sanchezmurillo
+ * CST-150
+ * Activity 11
+ * 06 AUG 2022
+ */
+namespace Activity_11
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * Here I have declared static variables that I am going to use 
+         * in my method, they are accessable by all methods in this class.
+         */ 
+        static int rollsCounted = 0;
+        static Boolean snakeEyes;
+        //I had to create one instance of random becase I kept getting the same #
+        static Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Event handler for the Roll Dice Button
+        private void RollDiceButton_Click(object sender, EventArgs e)
+        {
+            //Declare face for both dice
+            int diceFaceOne;
+            int diceFaceTwo;
+            //Here we invoke both methods to get the face value
+                diceFaceOne = RollDiceOne();
+                diceFaceTwo = RollDiceTwo();
+            //Here we set the label to display the face values
+            Dice1Label.Text = diceFaceOne.ToString();
+            Dice2Label.Text = diceFaceTwo.ToString();
+            //Here invoke our method to check for snake eyes.
+                snakeEyes = checkSnakeEyes(diceFaceOne, diceFaceTwo);
+            //Increments the rolls counter so we can display the quantity later
+                rollsCounted++;
+            //Below we have an if statement to handle the snake eyes being true.
+            if (snakeEyes)
+            {
+                //We display how many rolls it took and also reset the values
+                MessageBox.Show("It took you " + rollsCounted + " rolls to get snake eyes!");
+                rollsCounted = 0;
+                snakeEyes = false;
+            }
+        }
+        //Method to create the Dice One Face value
+        public int RollDiceOne()  {
+            int diceFaceOne = random.Next(1,6);
+            return diceFaceOne;
+        }
+        //Creates Dice Two Face Value
+        public int RollDiceTwo() {
+            int diceFaceTwo = random.Next(1,6);
+            return diceFaceTwo;
+        }
+        //Checks to see if the snake eyes event is true and changes the boolean value
+        public Boolean checkSnakeEyes(int diceFaceOne, int diceFaceTwo)
+        {
+            if (diceFaceOne == 1 && diceFaceTwo == 1)
+            {
+                return true;
+            }
+            else return false;
+        }
+        //Exit Button
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+ }
+
